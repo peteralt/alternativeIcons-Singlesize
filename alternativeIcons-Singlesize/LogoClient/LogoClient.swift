89f1bc500e95850848ada5d3520c6ce70cbd87c4
Peter Alt <peter@peteralt.com>
2023-01-19 14:40:56 -0700

    some documentation

--- alternativeIcons-Singlesize/LogoClient/LogoClient.swift
+++ alternativeIcons-Singlesize/LogoClient/LogoClient.swift
@@ -2,9 +2,15 @@ import Foundation
 import UIKit
 
 struct AlternativeIcon: Equatable, Hashable, Identifiable {
+    /// Optional asset name, this identifies a custom icon. Use `nil` for the original icon.
     let iconName: String?
+    
+    /// User friendly title for the icon
     let title: String
+    
+    /// This isn't a proper abstraction, rather a shorthand to access the asset.
     let icon: UIImage
+    
     var id: String {
         title
     }
