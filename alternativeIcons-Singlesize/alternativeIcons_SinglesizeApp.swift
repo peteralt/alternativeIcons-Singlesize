ad289a209cc68713588ba570bcfa28a94eff5be8
Peter Alt <peter@peteralt.com>
2023-01-19 14:18:59 -0700

    Initial Commit

---
+++ alternativeIcons-Singlesize/alternativeIcons_SinglesizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  alternativeIcons_SinglesizeApp.swift
+//  alternativeIcons-Singlesize
+//
+//  Created by Peter Alt on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct alternativeIcons_SinglesizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
